d4102f6b4be2922721ba8a73ad2f8229f11cb05e
Eric-11 <svsarana@yahoo.com>
2020-04-18 15:48:11 -1000

    added randomness and bug fix
    2 bugs corrected (see model.py commits). Changed the display of the text data. Removed pprint.

--- monitor.py
+++ monitor.py
@@ -20,7 +20,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import model
-import pprint
 
 #
 # Notes on Parameters
@@ -73,6 +72,7 @@ import pprint
 # NOT COMPLETED: tidal volume (Tv)  and transpulmonary pressure (Pl)
 #
 
+
 class MONITOR:
 
     models = ''
@@ -85,6 +85,9 @@ class MONITOR:
     peak_idx = 0
     bps = 0
     max_time = 0
+    threshold = 0  # trigger level for breath cycle
+    threshold_factor = 1.25
+    random = [0, 0, 0]  # percentage range for random varation in simulations
 
     # Stats taken from:
     # The basics of respiratory mechanics: ventilator-derived parameters
@@ -113,11 +116,18 @@ class MONITOR:
     def __init__(self):
         print("Setting up model.")
         self.models = model.BREATH()
-        self.models.scale(bpm=30, peak=31, peep=5)
+        self.scale_model()  # add in peep, bpm and peak pressure point
+
+    def enable_random(self, random=[0, 0, 0]):
+        self.random = random
+
+    def scale_model(self, bpm=30, peak=31, peep=5):
+        self.models.scale(bpm, peak, peep)
+        self.threshold = peep * self.threshold_factor
 
     def plot(self):
         """
-        Plots the sampled data from the model
+        Plots the sampled data
         """
         fig = plt.figure()
         ax1 = fig.add_subplot(1, 1, 1)
@@ -147,23 +157,28 @@ class MONITOR:
         # convert to scalar from 2d array of [time, pressure]
         self.peak_idx = peak_idx[1]
         self.peep_min = peep_min[1]  # floor of pressure readings (PEEP)
-        self.bps = 1 / peak[0]  # bps  - data has one breath
         self.peak = peak[1]
         self.max_time = peak[0]
 
-        threshold = self.peep_min * 1.25  # factor for trigger
+        if self.threshold == 0:
+            threshold = self.peep_min * self.threshold_factor  # factor for trigger
+        else:
+            threshold = self.threshold
+
+        self.threshold = threshold
         self.find_cycles(threshold)  # find start/end points of each cycle
+
         if plot:
             self.fig = plt.figure()
 
         for i in range(0, int(len(self.captured) / 2)):
             self.contours(i, threshold, plot)  # find points in cycle i
-            self.cycle_stats.append(self.stats) # build list of dictionary stats
-        pprint.pprint(self.cycle_stats)
+            self.cycle_stats.append(self.stats)  # build list of dictionary stats
         if plot:
+            plt.ylim(bottom=-3)  # return the current ylim
             plt.show()
 
-    def find_cycles(self, threshold=10):
+    def find_cycles(self, threshold):
         """
         Sweeps through captured data and finds breath cycles
 
@@ -217,9 +232,8 @@ class MONITOR:
         Note the point of detecting where slope flattens is based on finding
         where the value is 1/factor of peak slope, where factor = the divisor
         """
-        # @TODO this method needs heavy refactoring
         
-        factor = 5  # divisor of peak slope to determine when it is flattening
+        factor = 10  # divisor of peak slope to determine when it is flattening
 
         if len(self.captured) % 2 > cycle:
             # requested cycle doesn't exist in waveform
@@ -232,18 +246,14 @@ class MONITOR:
         # section off data for analysis, may need to widen start/stop points
         # for a bigger window
         self.datanp = np.array(self.data)  # copy over cycle to analyze
-        self.datanp = self.datanp[start:end, :]  # slice cycle
+        data_cycle = self.datanp[start:end, :]  # slice cycle
 
-        peak = np.amax(self.datanp, axis=0)[1]
-        peak_idx = np.argmax(self.datanp, axis=0)[1]
-        peep_min = np.amin(self.datanp, axis=0)[1]
-        ave = np.average(self.datanp, axis=0)[1]
-        max_time = self.datanp[-1][0] - self.datanp[0][0]  # length of cycle
-
-        # Use the differential to detect slope
-        diff = np.diff(self.datanp[:, 1])
+        peak = np.amax(data_cycle, axis=0)[1]
+        peak_idx = np.argmax(data_cycle, axis=0)[1]
+        peep_min = np.amin(data_cycle, axis=0)[1]
 
         if plot:
+            # plot full waveform
             ax1 = self.fig.add_subplot(1, 1, 1)
             xar = self.data[:1]
             xar = []
@@ -258,15 +268,11 @@ class MONITOR:
             if cycle == 0:
                 ax1.plot(yar, xar)
                 ax1.plot([yar[0], yar[-1]], [threshold, threshold], 'r:')
-                ax1.text(yar[int(len(yar) / 2)], threshold, "Threshold")
+                ax1.text(yar[int(len(yar) / 2)], threshold, "Threshold {:1.1f}".format(threshold))
                 for i in range(0, len(self.captured), 2):
                     ax1.text(self.captured[i][0], 0, "S")  # Start cycle
                     ax1.text(self.captured[i + 1][0],
                              0,"E", horizontalalignment='right')  # End cycle
-
-            # average - not used
-            #ax1.plot([self.datanp[0][0], self.datanp[-1][0]], [ave, ave],
-            #         'c--')
 
         # Inhalation and Exhalation Detection Method
         #
@@ -276,6 +282,19 @@ class MONITOR:
         # bracket the two min and maxs by looking for when the slope
         # approaches a scaled value of the maximum
 
+        # Use the differential to detect slope
+        # this is a descrete calculation of just (x1-x0)/(y1-y0),
+        # (x2-x1)/(y2-y1)...(xn-xn-1)/(yn - yn-1)
+        x0=0
+        y0=0
+        diff=[]
+        for x,y in data_cycle:
+            m=(y-y0)/(x-x0)
+            x0=x
+            y0=y
+            diff.append(m)
+        diff=np.array(diff)
+ 
         # Analyze derivative for slope changes
         # drop one y-axis point for plotting differential
         diff_max = np.amax(diff)  # max value
@@ -285,13 +304,13 @@ class MONITOR:
 
         if plot:
             # show peak
-            ax1.plot([self.datanp[peak_idx][0]], [peak], 'ro')
-            ax1.text(self.datanp[peak_idx][0], peak * 1.02, "Peak")
+            ax1.plot([data_cycle[peak_idx][0]], [peak], 'ro')
+            ax1.text(data_cycle[peak_idx][0], peak * 1.02, "Peak {:2.1f}".format(peak))
 
-            # Show differential graph for cycle
-            yar.pop()  # diff has one less element than yar
-            ax1.plot(yar[start:end - 1], diff)
-            ax1.plot([yar[start],yar[-1]], [0,0])  # reference crossings
+            # Show differential crossings cycle
+            yar.pop()    
+            # ax1.plot(yar[start:end], diff)
+            # ax1.plot([yar[start],yar[-1]], [0,0])  # reference crossings
 
         # Find end of rising inhalation
         # search between max diff and min diff point for slope changes
@@ -303,9 +322,8 @@ class MONITOR:
         j = x  # this might now be the start of exhalation or peak decay
 
         if plot:
-            ax1.plot([self.datanp[i][0], self.datanp[i][0]], [0, peak],
+            ax1.plot([data_cycle[i][0], data_cycle[i][0]], [0, peak],
                      'r:')  # start of rise inhalation
-            # in special case Pplat might be wrong and equal to peak
 
         # search between max diff and min diff point for slope changes
         # look for where the slope drops by a factor of the peak
@@ -315,12 +333,12 @@ class MONITOR:
         j2 = x  # possible end of exhalation cycle
 
         if plot:
-            ax1.plot([self.datanp[x][0], self.datanp[x][0]], [0, peak],
+            ax1.plot([data_cycle[x][0], data_cycle[x][0]], [0, peak],
                      'r-')  # end of rise of inhalation
 
         # search after min diff point for slope changes
         # look for where the slope drops by a factor of the peak
-        for x in range(j, len(self.datanp)):
+        for x in range(j, len(data_cycle)):
             if diff[x] >= diff_min / factor:  # slope slows by this factor
                 break
         diff_min_idx = x  # index of the end of the exhalation slope
@@ -329,21 +347,21 @@ class MONITOR:
         ################################################################
         # Special Case: large peak causes false detection of exhalation
         #
-        # Check remaining waveform for another min for sanity check
+        # Check remaining waveform for another minimum for sanity check
         ################################################################
         # move over few samples if possible from peak to start search
-        x_spec = x
+        x_spec = x  # bracket off remaining cycle from minimum
         if x + 10 < len(diff):
             x_spec = x + 10
         diff_min_special = np.amin(diff[x_spec:])
-        diff_min_special_idx = np.argmin(diff[x_spec:]) + x_spec # second dip found
+        diff_min_special_idx = np.argmin(diff[x_spec:]) + x_spec  # second dip found
 
-        use_special_case=False
+        use_special_case = False
         # is it similar in magnitude? +/- 0.3
-        if diff_min_special != 0:
-            if (diff_min/diff_min_special)<1.3 or (div_min/diff_min_special)>0.7:
+        if diff_min_special != 0 and diff_min_special != diff_min:
+            if (diff_min / diff_min_special) < 1.3 and (diff_min / diff_min_special) > 0.7:
                 use_special_case = True
-                # could also add check to see Ppeak close or eqaul to Pplat
+                # could also add time window check to see Ppeak close or equal to Pplat
 
         # reverse from minimum and search for start of slope
         # by finding where slope drops by a factor of the peak
@@ -353,7 +371,7 @@ class MONITOR:
         diff_min_special_idx = x   # index of the end of the exhalation slope
 
         # look for where the slope drops by a factor of the peak
-        for x in range(x_spec, len(self.datanp)):
+        for x in range(x_spec, len(data_cycle)):
             if diff[x] >= diff_min_special / factor:  # slope slows by this factor
                 break
         diff_min_special_idx2 = x + x_spec  # index of the end of the exhalation slope
@@ -362,15 +380,26 @@ class MONITOR:
             j = diff_min_special_idx
             j2 = diff_min_special_idx2
 
+        # second minimum limit checks
+        # will plot exhale lines to end of cycle if error occurs
+        # this is for checking this condition and checking algorithm sanity
+        if j>len(data_cycle):
+            print("{} j too big".format(j))
+            j=len(data_cycle) - 10
+        if j2>len(data_cycle):
+            print("{} j2 too big".format(j2))
+            j2=len(data_cycle) - 1
+            
+
         if plot:
-            ax1.plot([self.datanp[j][0], self.datanp[j][0]], [0, peak],
+            ax1.plot([data_cycle[j][0], data_cycle[j][0]], [0, peak],
                      'g:')  # second minium found -- start
-            ax1.plot([self.datanp[j2][0], self.datanp[j2][0]], [0, peak],
+            ax1.plot([data_cycle[j2][0], data_cycle[j2][0]], [0, peak],
                      'g-')  # second minium found -- end
 
         # Plot Plateau
-        ax1.plot(self.datanp[j][0], self.datanp[j][1], 'r*')
-        ax1.text(self.datanp[j][0], self.datanp[j][1], "Pplat")
+        ax1.plot(data_cycle[j][0], data_cycle[j][1], 'r*')
+        ax1.text(data_cycle[j][0], data_cycle[j][1], "Pplat {:2.1f}".format(data_cycle[j][1]))
 
         # inspiry time -- trigger time to peak time
         # self.datanp[peak_idx][0] - self.datanp[i][0]
@@ -386,23 +415,24 @@ class MONITOR:
 
         # Find P01 100ms into inhalation
         # linear interpolation to find value
-        P01 = np.interp(self.data[start][0] + 0.1, self.datanp[:, 0], self.datanp[:, 1])
+        P01 = np.interp(self.data[start][0] + 0.1,
+                        data_cycle[:, 0], data_cycle[:, 1])
         if plot:
             ydelta = (self.data[start+4][0] - self.data[start][0])/5
             jump = int(0.1/ydelta)  # estimate index @ 0.1s for plotting
             ax1.plot([self.data[start+jump][0]], [P01], 'ro')
-            ax1.text(self.data[start+jump][0], P01, "P0.1")
+            ax1.text(self.data[start+jump][0], P01, "P0.1 {:2.1f}".format(P01))
 
         # Find PTP for cycle
-        PTPavg = np.average(self.datanp[0:peak_idx], axis=0)[1]
+        PTPavg = np.average(data_cycle[0:peak_idx], axis=0)[1]
         if plot:
             text = "PTP={:2.1f}".format(PTPavg)
             ax1.text(self.data[int(start+peak_idx/2)][0], peep_min, text,
                      verticalalignment='top')
 
         # I:E ratio
-        nomin1 = (self.datanp[peak_idx][0] - self.datanp[i][0]) + (self.datanp[j][0] - self.datanp[peak_idx][0])
-        denom1 =  (self.datanp[diff_min_idx][0] - self.datanp[j][0]) + (self.datanp[-1][0] - self.datanp[diff_min_idx][0])
+        nomin1 = (data_cycle[peak_idx][0] - data_cycle[i][0]) + (data_cycle[j][0] - data_cycle[peak_idx][0])
+        denom1 =  (data_cycle[diff_min_idx][0] - data_cycle[j][0]) + (data_cycle[-1][0] - data_cycle[diff_min_idx][0])
         denom1 = denom1/nomin1
         ie_text = "1:{:1.1f}".format(denom1)
         if plot:
@@ -424,27 +454,28 @@ class MONITOR:
                      verticalalignment='bottom')
             
         # Populate stats dictionary
-        self.stats["PEEP"] = peep_min
-        self.stats["PEEPi"] = peepi
-        self.stats["Ppeak"] = peak
-        self.stats["FlowI"] = self.datanp[peak_idx][0] - self.datanp[i][0]
-        self.stats["Ipause"] = self.datanp[j][0] - self.datanp[peak_idx][0]
-        self.stats["FlowE"] = self.datanp[j2][0] - self.datanp[j][0]
-        self.stats["Epause"] = self.datanp[-1][0] - self.datanp[j2][0]
+        self.stats={} # initalize dict each time to avoid passing same reference to lists
+        self.stats["PEEP"] = round(peep_min, 2)
+        self.stats["PEEPi"] = round(peepi, 2)
+        self.stats["Ppeak"] = round(peak, 2)
+        self.stats["FlowI"] = round(data_cycle[peak_idx][0] - data_cycle[i][0], 2)
+        self.stats["Ipause"] = round(data_cycle[j][0] - data_cycle[peak_idx][0], 2)
+        self.stats["FlowE"] = round(data_cycle[j2][0] - data_cycle[j][0], 2)
+        self.stats["Epause"] = round(data_cycle[-1][0] - data_cycle[j2][0], 2)
         self.stats["I:E"] = ie_text
-        self.stats["Pplat"] = self.datanp[j][1]
-        self.stats["Start"] = self.data[start][0]
-        self.stats["End"] = self.data[end][0]
+        self.stats["Pplat"] = round(data_cycle[j][1], 2)
+        self.stats["Start"] = round(self.data[start][0], 2)
+        self.stats["End"] = round(self.data[end][0], 2)
         self.stats["Vt"] = 0      # not done
-        self.stats["dP"] = self.datanp[j][1] - peep_min
+        self.stats["dP"] = round(data_cycle[j][1] - peep_min, 2)
         self.stats["Pl"] = 0      # not done
-        self.stats["P01"] = P01
-        self.stats["PTP"] = PTPavg
-        self.stats["RR"] = (1 / (self.data[end][0] - self.data[start][0])) * 60
-
+        self.stats["P01"] = round(P01, 2)
+        self.stats["PTP"] = round(PTPavg, 2)
+        self.stats["RR"] = round((1 / (self.data[end][0] - self.data[start][0])) * 60, 2)
+            
     def get_sample(self, time):
-        """ Retrivies single sample with specified time index """
-        point = self.models.get_simulated_data(time)
+        """ Retrivies single sample from model with specified time index """
+        point = self.models.get_simulated_data(time, self.random)
         self.data.append([time, point])
 
     def read_pressure(self, seconds, delay=0.01):
@@ -457,9 +488,81 @@ class MONITOR:
             time.sleep(delay)  # wait for sample
             self.get_sample(timer)
             timer = timer + delay
-        
 
+    def track_breath(self, cycles=1, timeout=10,
+                     sample_rate=0.01, max_peak=35):
+        """ Tracks breath cycles in real time
+ 
+        Parameters:
+        cycles (int): number of cycles (breaths) to track then analyze
+        timeout (int): seconds to search for pattern before aborting @TODO
+        sample_rate (float): seconds between pressure samples
+        max_peak (float): if measured above this value generate alarm @TODO
+
+        Returns:
+        bool: returns 0 for failure, 1 for success, data is moved into self.data
+        and self.cycle_stats
+        """
+
+        buffer = []
+        sample_time = 0.0
+        breath_cnt = 0
+        state = 0  # no trigger
+
+        # loop to sample data
+        print("Sampling sensor {} cycles :: {} sample rate :: {} threshold"
+              .format(cycles, sample_rate, self.threshold))
+        while breath_cnt < cycles:   # no timeout yet
+            time.sleep(sample_rate)  # wait for sample - @TODO use RTC elapsed
+            point = self.models.get_simulated_data(sample_time, self.random)  # simulated
+            buffer.append([sample_time, point])
+            sample_time = sample_time + sample_rate
+
+            if point > max_peak:
+                print("ALERT! Max Pressure Limit Exceeded!")
+                # @TODO
+                # alarm UI and adjust motor mechanism if necessary
+
+            # Test for threshold crossings
+            if state == 0:
+                # waiting for trigger positive
+                if len(buffer) > 10 and breath_cnt == 0:
+                    # slice of last 10 samples to try to place start cycle in
+                    # same time window every so waveform looks triggered
+                    buffer = buffer[-10:]
+                if point > self.threshold:
+                    state = 1
+                    continue
+            if state == 1:
+                # rising, wait for fall
+                if point < self.threshold:
+                    state = 2
+                    continue
+            if state == 2:                # fallen, wait for start of next rise
+                if point > self.threshold:
+                    state = 0
+                    breath_cnt = breath_cnt + 1
+        self.data = buffer
+        self.compute()  # process samples
+        return True
+
+    def print(self):
+        i = 0
+        for item in self.cycle_stats:
+            print("Cycle #{}:".format(i))
+            i += 1
+            for key, value in item.items():
+                print("{:>10} = {}".format(key, value))
+
+                    
 if __name__ == "__main__":
     mon = MONITOR()
+    mon.enable_random(random=[10, 10, 10])  # note bpm randomness not working now
+    
+    #mon.track_breath(cycles=2, max_peak=50)
+    #mon.print()
+    
+    # test for long sample of time
     mon.read_pressure(seconds=7, delay=0.01)
     mon.compute(plot=True)  # setting plot to true will show the data graph
+    mon.print()
